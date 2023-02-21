402693b80c7de5f06fce7f8e41bbb02faeec29aa
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-02-20 23:10:40 -0500

    intro a Custom Hooks

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,22 +1,28 @@
 import React, {useState, useEffect} from 'react'
 import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
     
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
 
-    useEffect( () => {
-        getGifs(category)
-        .then(setImages)
-    }, [category])
+    const {loading} = useFetchGifs();
 
+
+    // useEffect( () => {
+    //     getGifs(category)
+    //     .then(setImages)
+    // }, [category])
 
 
   return (
     <>
     <h3> {category} </h3>
-    <div className='card-grid'>
+
+        
+    {loading ? 'Cargando...' : 'Data Cargada'} 
+    {/* <div className='card-grid'>
         
 
  
@@ -27,7 +33,7 @@ export const GifGrid = ({category}) => {
                     {...img}  />
                 ))
             }
-    </div>
+    </div> */}
     </>
   )
 }
