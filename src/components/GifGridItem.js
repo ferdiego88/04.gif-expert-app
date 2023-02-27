d8ae1ecf64008139fb100eed185485f91fe9e1f7
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-02-27 12:39:39 -0500

    pruebas in GifGrid

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const GifGridItem = ({id,title,url}) => {
+const GifGridItem = ({title,url}) => {
 
   return (
     <div className='card animate__animated animate__fadeIn'>
@@ -9,6 +10,11 @@ const GifGridItem = ({id,title,url}) => {
         {}
     </div>
   )
+}
+
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 }
 
 export default GifGridItem
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -2,12 +2,38 @@ import { shallow }from 'enzyme'
 import React from 'react';
 import GifGridItem from "../../components/GifGridItem";
 
-describe('Pruebas en <GifGridItem />', () => {
+describe('Expects <GifGridItem />', () => {
 
-    test('debe de mostrar <GifGridItem /> correctamente', () => { 
+    const title = 'GifGridItem titulo';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow( <GifGridItem title={title} url={url}/>);
 
-        const wrapper = shallow( <GifGridItem />);
+
+    test('should be to show <GifGridItem /> successfully', () => { 
+
 
         expect(wrapper).toMatchSnapshot();
      })
+
+     test('should be a paragraph with the title', () => {
+
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+     })
+
+     test('should have a image and url equals to props', () => {
+
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+
+      })
+
+      test('should be have animate__fadeIn', () => {
+
+            const fadeIn = 'animate__fadeIn';
+            const div = wrapper.find('div');
+            
+            expect(div.prop('className')).toContain(fadeIn);
+       })
 })
